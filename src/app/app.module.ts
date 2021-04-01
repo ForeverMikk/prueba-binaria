a87514dd3597661756863ffb094881f4ed9626be
Michael Molina <michaelmolina679@gmail.com>
2021-04-01 14:37:51 -0600

    Terminando Parte 1 de la prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { LoginComponent } from './components/login/login.component';
+import { UserComponent } from './components/user/user.component';
+
+// import { Client } from 'minio';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginComponent, UserComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { NavigationExtras, Router } from '@angular/router';
+
+import axios from 'axios';
+// import { Client } from 'minio';
+// import * as Minio from 'minio';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  private url = 'http://ec2-54-187-122-255.us-west-2.compute.amazonaws.com/';
+  loginUrl = this.url + 'api/login2';
+  logoutUrl = this.url + 'api/logout';
+  profileUrl = this.url + 'api/users/';
+
+  userData: any;
+  userToken = '';
+
+  file = './../../assets/niclas-illg-wzVQp_NRIHg-unsplash.jpg';
+
+  // s3Client = new Minio.Client({
+  //   endPoint: '',
+  //   accessKey: '',
+  //   secretKey: '',
+  // });
+
+  // console.log(this.client);
+  constructor(private router: Router) {
+    // try {
+    //   const client = new Client({
+    //     endPoint: 's3.binahriaanalytics.ninja',
+    //     accessKey: 'S3_Binahria_T',
+    //     secretKey: 'uVgsuanjRDhvyjIoNnwf',
+    //   });
+    //   console.log(client);
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  }
+
+  ngOnInit(): void {
+    // console.log(client);
+  }
+
+  async onSubmit() {
+    event?.preventDefault();
+    // console.log(this.loginForm);
+
+    try {
+      const { data } = await axios.post(this.loginUrl, this.loginForm.value);
+      console.log('Datos de Sesión: ', data);
+
+      this.userToken = data.access_token;
+
+      localStorage.setItem('userToken', this.userToken);
+
+      this.router.navigate(['user']);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // listFile() {
+  //   this.client.listBuckets(function (err, buckets) {
+  //     if (err) return console.log(err);
+
+  //     console.log('buckets: ', buckets);
+  //   });
+  // }
+
+  // uploadFile() {
+  //   this.client.makeBucket('newbucket', 'us-east-1', function (err) {
+  //     if (err) return console.log('Error creating bucket.', err);
+
+  //     console.log('Bucket created successfully in "us-east-1".');
+  //   });
+  // }
+}
